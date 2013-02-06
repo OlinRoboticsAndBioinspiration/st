f0fb1af7c61ebcb2f85fee3f437bbf2dc0e3c5c3
Shivam Desai <shivamxdesai@gmail.com>
2013-02-06 10:36:12 -0500

    Added wrappers for WHERE, DECIMAL, JOINT, HAND, and MOVE_HAND

--- st.py
+++ st.py
@@ -6,20 +6,24 @@ import re
 DEFAULT_DEV = '/dev/tty.KeySerial1'
 
 # Use this one for PC
-DEFAULT_DEV_PC = 'COM#'
+DEFAULT_DEV_PC = 'COM3'
 DEFAULT_BAUD_RATE = 19200
 
+# Roboforth Strings
 CR = '\r'
 LF = '\n'
 
 PURGE = 'PURGE'
 ROBOFORTH = 'ROBOFORTH'
+DECIMAL = 'DECIMAL'
 START = 'START'
+JOINT = 'JOINT'
 CALIBRATE = 'CALIBRATE'
 HOME = 'HOME'
+WHERE = 'WHERE'
 CARTESIAN = 'CARTESIAN'
 MOVETO = 'MOVETO'
-
+HAND = 'HAND'
 
 class StArm():
     '''Class for controlling the 5-axis R17 arm from ST Robotics'''
@@ -36,13 +40,15 @@ class StArm():
         connected to.
     '''
 
-    def __init__(self, dev=DEFAULT_DEV, baud=DEFAULT_BAUD_RATE):
+    def __init__(self, dev=DEFAULT_DEV_PC, baud=DEFAULT_BAUD_RATE):
         self.cxn = s.Serial(dev, baudrate=baud, timeout=3)
         # TODO 
         # Check and parse return values of all ROBOFORTH methods called. 
         self.wait_read()
         self.purge()
         self.roboforth()
+        self.decimal()
+        self.joint()
         self.start()
         self.calibrate()
         self.home()
@@ -51,19 +57,32 @@ class StArm():
     def purge(self):
         print('Purging...')
         self.cxn.write(PURGE + CR)
+        t.sleep(3)
         self.check_result(PURGE)
 
     def roboforth(self):
         print('Starting RoboForth...')
         self.cxn.write(ROBOFORTH + CR)
-        t.sleep(3)
+        t.sleep(2)
         self.check_result(ROBOFORTH)
 
+    def decimal(self):
+		print('Setting decimal mode...')
+		self.cxn.write(DECIMAL + CR)
+		t.sleep(2)
+		self.check_result(DECIMAL)
+	
     def start(self):
         print('Starting...')
         self.cxn.write(START + CR)
         t.sleep(3)
         self.check_result(START)
+
+    def joint(self):
+		print('Setting Joint mode...')
+		self.cxn.write(JOINT + CR)
+		t.sleep(2)
+		self.check_result(JOINT)
 
     def calibrate(self):
         print('Calibrating...')
@@ -82,6 +101,12 @@ class StArm():
         self.cxn.write(CARTESIAN + CR)
         t.sleep(2)
         self.check_result(CARTESIAN)
+        
+    def hand(self):
+		print('Controlling hand...')
+		self.cxn.write(HAND + CR)
+		t.sleep(2)
+		self.check_result(HAND)
 
     def check_result(self, cmd):
         while(self.cxn.inWaiting() == 0):
@@ -93,7 +118,6 @@ class StArm():
         else:
             print result
             raise RuntimeError(cmd + ' command failed.')
-
 
     def wait_read(self):
         print(str(self.cxn.inWaiting()))
@@ -113,10 +137,19 @@ class StArm():
         self.cxn.write(str(x) + ' ' + str(y) + ' ' + str(z) + ' MOVETO' + CR)
         t.sleep(5)
         self.check_result(MOVETO)
+        
+    def move_hand(self, roll):
+		self.joint()
+		self.hand()
+		print('Moving hand to ' + str(roll) + '...')
+		self.cxn.write(str(roll) + ' MOVETO' + CR)
+		t.sleep(5)
+		self.check_result(MOVETO)
+		self.cartesian()
 
-
-
-
-
-
-
+    def where(self):
+		print('Obtaining robot coordinates...')
+		self.cxn.write(WHERE + CR)
+		t.sleep(2)
+		print (self.cxn.read(self.cxn.inWaiting()))
+		self.check_result(WHERE)
