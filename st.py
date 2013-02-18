dc2f952efed4415c86d9684caa163965714eab66
Aaron Hoover <amhoov@gmail.com>
2013-02-18 10:44:17 -0500

    Removed all check_result calls from commands.

--- st.py
+++ st.py
@@ -76,10 +76,9 @@ class StArm():
             self.joint()
             self.start()
             self.calibrate()
-            self.home(True)
-            t.sleep(2)
+            self.home()
             self.cartesian()
-        
+
         try:
             (cp, pp) = self.where()
             self.curr_pos = StPosCart(cp)
@@ -92,63 +91,40 @@ class StArm():
     def purge(self):
         print('Purging...')
         self.cxn.write(PURGE + CR)
-        t.sleep(3)
-        self.check_result(PURGE)
 
     def roboforth(self):
         print('Starting RoboForth...')
         self.cxn.write(ROBOFORTH + CR)
-        t.sleep(2)
-        self.check_result(ROBOFORTH)
 
     def decimal(self):
 		print('Setting decimal mode...')
 		self.cxn.write(DECIMAL + CR)
-		t.sleep(2)
-		self.check_result(DECIMAL)
-	
+
     def start(self):
         print('Starting...')
         self.cxn.write(START + CR)
-        t.sleep(3)
-        self.check_result(START)
 
     def joint(self):
 		print('Setting Joint mode...')
 		self.cxn.write(JOINT + CR)
-		t.sleep(2)
-		self.check_result(JOINT)
 
     def calibrate(self):
         print('Calibrating...')
         self.cxn.write(CALIBRATE + CR)
-        t.sleep(20)
-        self.check_result(CALIBRATE)
 
-    def home(self, check_result=False):
+    def home(self):
         print('Homing...')
         self.cxn.write(HOME + CR)
-        if check_result:
-            self.check_result(HOME)
 
     def cartesian(self):
         print('Setting mode to Cartesian...')
-        t.sleep(3)
-        print("Here's the buffer contents: " + self.cxn.read(self.cxn.inWaiting()))
-        self.cxn.flushInput()
         self.cxn.write(CARTESIAN + CR)
-        t.sleep(2)
-        self.check_result(CARTESIAN)
-        
+
     def hand(self):
 		print('Controlling hand...')
 		self.cxn.write(HAND + CR)
-		t.sleep(2)
-		self.check_result(HAND)
 
     def check_result(self, cmd):
-        while not self.cxn.inWaiting():
-            pass
         result = self.cxn.read(self.cxn.inWaiting())
         if result[-5:-3] == 'OK':
             print('Command ' + cmd + ' succeeded.')
@@ -198,36 +174,29 @@ class StArm():
         if self.get_accel() == accel:
             print('Acceleration successfully set to %d' % accel)
         else:
-            print('Failed to set acceleration!')        
+            print('Failed to set acceleration!')
 
     def move_to(self, x, y, z, check_result=False, pitch=None, roll=None):
         self.cartesian()
         print('Moving to cartesian coords: (' + str(x) + ', ' + str(y) + ', ' + \
         str(z) + ')')
         self.cxn.write(str(x) + ' ' + str(y) + ' ' + str(z) + ' MOVETO' + CR)
-        if(check_result):
-            t.sleep(5)
-            self.check_result(MOVETO)
-    
+
     def rotate_wrist(self, roll):
         print('Rotating wrist to %s' % roll)
         self.cxn.write(TELL + ' ' + WRIST + ' ' + str(roll) + ' ' + MOVETO + CR)
-        self.check_result(MOVETO)
 
     def rotate_wrist_rel(self, roll_inc):
         print('Rotating wrist by %s.' % roll_inc)
         self.cxn.write(TELL + ' ' + WRIST + ' ' + str(roll_inc) + ' ' + MOVE + CR)
-        self.check_result(MOVE)
 
     def rotate_hand(self, pitch):
         print('Rotating hand to %s.' %pitch)
         self.cxn.write(TELL + ' ' + HAND + ' ' + str(pitch) + ' ' + MOVETO + CR)
-        self.check_result(MOVETO)
 
     def rotate_hand_rel(self, pitch_inc):
         print('Rotating hand by %s' % pitch_inc)
         self.cxn.write(TELL + ' ' + HAND + ' ' + str(pitch_inc) + ' ' + MOVE + CR)
-        self.check_result(MOVE)
 
     def move_hand(self, roll):
 		self.rotate_hand(roll)
@@ -235,14 +204,11 @@ class StArm():
     def energize(self):
         print('Powering motors...')
         self.cxn.write(ENERGIZE + CR)
-        t.sleep(2)
         self.check_result(ENERGIZE)
 
     def de_energize(self):
         print('Powering down motors...')
         self.cxn.write(DE_ENERGIZE + CR)
-        t.sleep(2)
-        self.check_result(DE_ENERGIZE)
 
     def where(self):
         self.cxn.flushInput()
