a1937cbd710fcb0a73c458c7b3f0d8eae98c4415
amhoov <amhoov@gmail.com>
2014-03-21 11:52:12 -0400

    Fixed unreliability of block_on_cmd method

--- st.py
+++ st.py
@@ -1,6 +1,6 @@
 import serial as s
-import re
 import shlex
+import time
 
 # Use this one for Mac/Linux
 DEFAULT_DEV = '/dev/tty.KeySerial1'
@@ -8,7 +8,7 @@ DEFAULT_DEV = '/dev/tty.KeySerial1'
 # Use this one for PC
 DEFAULT_DEV = 'COM7'
 DEFAULT_BAUD_RATE = 19200
-DEFAULT_TIMEOUT = 0.05
+DEFAULT_TIMEOUT = 0.1
 
 # Roboforth Strings
 CR = '\r'
@@ -53,10 +53,11 @@ class StPosCart():
         self.roll = pos[4]
 
     def __repr__(self):
-        return '(x=%s, y=%s, z=%s, pitch=%s roll=%s)' % (self.x, self.y,
-                                                         self.z,
-                                                         self.pitch,
-                                                         self.roll)
+        return 'X=%smm, Y=%smm, Z=%smm, Pitch=%sdeg, Roll=%sdeg' % (self.x/10.0,
+                                                         self.y/10.0,
+                                                         self.z/10.0,
+                                                         self.pitch/10.0,
+                                                         self.roll/10.0)
 
 
 class StArm():
@@ -70,17 +71,19 @@ class StArm():
         dev_name: The name of the serial device. For Macs/Linux, use
         /dev/tty.somestringofcharsandnums and for PCs use COMX where
         X is the COM port number the serial connector for the arm is
+In [36]: 
         connected to.
     '''
 
     def __init__(self, dev=DEFAULT_DEV, baud=DEFAULT_BAUD_RATE,
-                 init=True, to=DEFAULT_TIMEOUT):
+                 init=True, to=DEFAULT_TIMEOUT, debug=False):
         self.cxn = s.Serial(dev, baudrate=baud, timeout=to)
         # TODO
         # Check and parse return values of all ROBOFORTH methods called.
-        self.debug = False
+        self.debug = debug
         self.curr_pos = StPosCart()
         self.prev_pos = StPosCart()
+        self.block_timeout = 10
         if init:
             self.cxn.flushInput()
             self.purge()
@@ -146,20 +149,23 @@ class StArm():
 
     def cartesian(self):
         cmd = CARTESIAN
-        print('Setting mode to Cartesian...')
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
         self.block_on_result(cmd)
 
     def block_on_result(self, cmd):
+        t = time.time()
         s = self.cxn.read(self.cxn.inWaiting())
 
-        while re.search(OK, s) is None:
+        while s[-5:-3] != OK:
+            #Match '>' only at the end of the string
+            if s[-1:] == '>':
+                if self.debug:
+                    print('Command ' + cmd + ' completed without ' +
+                          'verification of success.')
+                raise Exception('Arm command failed to execute as expected.', s)
             s += self.cxn.read(self.cxn.inWaiting())
-            #if re.search('>', s) is not None:
-            #    print('Command ' + cmd + ' completed without ' +
-            #          'verification of success.')
-            #    return s
+
         if self.debug:
             print('Command ' + cmd + ' completed successfully.')
         return s
@@ -170,15 +176,12 @@ class StArm():
 
     def get_speed(self):
         cmd = SPEED + QUERY
-        print('Getting current speed setting...')
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
         result = self.block_on_result(cmd)
-        print(result)
         return int(result.split(' ')[-2])
 
     def set_speed(self, speed):
-        print('Setting speed to %d' % speed)
         cmd = str(speed) + ' ' + SPEED + IMPERATIVE
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
@@ -186,7 +189,6 @@ class StArm():
 
     def get_accel(self):
         cmd = ACCEL + QUERY
-        print('Getting current acceleration setting...')
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
         result = self.block_on_result(cmd)
@@ -199,32 +201,29 @@ class StArm():
         self.cxn.write(cmd + CR)
         self.block_on_result(cmd)
 
-    def move_to(self, x, y, z, block=True):
-        cmd = str(x) + ' ' + str(y) + ' ' + str(z) + ' MOVETO'
+    def move_to(self, pos, block=True):
+        cmd = str(pos[0]) + ' ' + str(pos[1]) + ' ' + str(pos[2]) + ' MOVETO'
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
         if block:
             self.block_on_result(cmd)
             self.where()
 
-    def move(self, del_x, del_y, del_z):
-        cmd = str(del_x) + ' ' + str(del_y) + ' ' + str(del_z) + ' MOVE'
+    def move(self, del_pos):
+        cmd = str(del_pos[0]) + ' ' + str(del_pos[1]) + ' ' + str(del_pos[2]) + ' MOVE'
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
-        if block:
-            self.block_on_result(cmd)
-            self.where()
+        self.block_on_result(cmd)
+        self.where()
 
     def rotate_wrist(self, roll):
         cmd = TELL + ' ' + WRIST + ' ' + str(roll) + ' ' + MOVETO
-        print('Rotating wrist to %s' % roll)
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
         self.block_on_result(cmd)
 
     def rotate_wrist_rel(self, roll_inc):
         cmd = TELL + ' ' + WRIST + ' ' + str(roll_inc) + ' ' + MOVE
-        print('Rotating wrist by %s.' % roll_inc)
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
         self.block_on_result(cmd)
@@ -233,23 +232,19 @@ class StArm():
 
     def rotate_hand(self, pitch):
         cmd = TELL + ' ' + HAND + ' ' + str(pitch) + ' ' + MOVETO
-        print('Rotating hand to %s.' % pitch)
-        self.cxn.flushInput()
-        self.cxn.write(cmd + CR)
-        self.block_on_result(cmd)
-        self.where()
-
-    def rotate_hand_rel(self, pitch_inc):
-        cmd = TELL + ' ' + HAND + ' ' + str(pitch_inc) + ' ' + MOVE
-        print('Rotating hand by %s' % pitch_inc)
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
         self.block_on_result(cmd)
         self.cartesian()
         self.where()
 
-    def move_hand(self, roll):
-        self.rotate_hand(roll)
+    def rotate_hand_rel(self, pitch_inc):
+        cmd = TELL + ' ' + HAND + ' ' + str(pitch_inc) + ' ' + MOVE
+        self.cxn.flushInput()
+        self.cxn.write(cmd + CR)
+        self.block_on_result(cmd)
+        self.cartesian()
+        self.where()
 
     def energize(self):
         cmd = ENERGIZE
@@ -265,6 +260,7 @@ class StArm():
         self.block_on_result(cmd)
 
     def where(self):
+        self.cartesian()
         cmd = WHERE
         self.cxn.flushInput()
         self.cxn.write(cmd + CR)
